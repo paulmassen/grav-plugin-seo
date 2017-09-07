e70938c2e78ea43c7f9aeab34c983abe9e1563ef
Paul <paul@massendari.com>
2017-09-07 08:49:08 +0000

    prevent error when fields not set

--- seo.php
+++ seo.php
@@ -110,7 +110,7 @@ class seoPlugin extends Plugin
              /**
              * Set Twitter Metatags
              */
-        if ($page->header()->twitterenable == 'true') {
+        if (isset($page->header()->twitterenable) and $page->header()->twitterenable == 'true') {
             
         
             if (isset($config['twitterid'])) {
@@ -143,7 +143,7 @@ class seoPlugin extends Plugin
             $meta['twitter:url']['property']  = 'twitter:url';
             $meta['twitter:url']['content']   = $page->url(true);
         }
-         if ($page->header()->facebookenable == 'true') {
+         if (isset($page->header()->facebookenable and $page->header()->facebookenable == 'true') {
          
                 $meta['og:sitename']['name']        = 'og:sitename';
                 $meta['og:sitename']['property']    = 'og:sitename';
