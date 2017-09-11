c0f6c08dedf9433dce7088a9722a41328baffead
Paul <paul@massendari.com>
2017-09-11 08:25:00 +0000

    fixed headline in article microdata

--- seo.php
+++ seo.php
@@ -375,7 +375,7 @@ class seoPlugin extends Plugin
        }
         }
      if (property_exists($page->header(),'articleenabled')){
-            if (property_exists($page->header(), 'article[headline]')){
+            if (isset($page->header()->article['headline'])){
                $headline =  $page->header()->article['headline'];
             } else {
                 $headline = $cleanTitle;
